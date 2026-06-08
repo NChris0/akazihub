9ffe7989fb15c784e02399edc71788d6f3074720
Nchris0 <ngarukiyintwali@gmail.com>
2026-06-08 01:55:08 -0700

    updated code

--- job-recruitment/src/App.jsx
+++ job-recruitment/src/App.jsx
@@ -24,33 +24,12 @@ function App() {
       <Route path="/register/employer" element={<EmployerRegister />} />
       <Route path="/login" element={<Login />} />
       <Route path="/categories" element={<CategoriesSection />} />
-      
-  <Route path="/" element={<Home />} />
-  <Route path="/login" element={<Login />} />
-
-  <Route
-    path="/jobseeker-dashboard"
-    element={<JobSeekerDashboard />}
-  />
-
-  <Route
-    path="/employer-dashboard"
-    element={<EmployerDashboard />}
-  />
-
-  <Route
-    path="/admin-dashboard"
-    element={<AdminDashboard />}
-  />
-  <Route
-  path="/post-job"
-  element={<PostJob />}
-/>
-<Route
-  path="/available-jobs"
-  element={<AvailableJobs />}
-/>
-</Routes>
+      <Route path="/jobseeker-dashboard" element={<JobSeekerDashboard />} />
+      <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/post-job" element={<PostJob />} />
+      <Route path="/available-jobs" element={<AvailableJobs />} />
+    </Routes>
     
      
     )
